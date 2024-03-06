32f5c16999913b75f008bd0924a1d514f29d6b1e
Ansh Kushwaha <ansh.kushwaha26@gmail.com>
2024-03-06 22:09:44 +0530

    Add working canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Canvas from './components/Canvas.tsx'
 
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='app-container fixed top-0 left-0 h-dvh w-dvw dark:bg-slate-950 bg-slate-50'>
+        <div className='canvas-container flex h-dvh w-[40%] justify-center items-center'>
+          <Canvas width={400} height={400} />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
