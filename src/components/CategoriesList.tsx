c8f5fb5206b141030b1d24300956a908fefc7b9e
Ansh Kushwaha <ansh.kushwaha26@gmail.com>
2024-03-31 16:23:12 +0530

    Regex update

--- src/components/CategoriesList.tsx
+++ src/components/CategoriesList.tsx
@@ -8,7 +8,7 @@ const CategoriesList = () => {
       <tbody>
         {categoryList.map((category: string, key: number) => (
           <tr key={key} className='p-2 m-2 flex justify-start font-mono capitalize shadow-xl w-[160px]'>
-            <td>{category.split('_').join(' ')}</td>
+            <td>{category.split(/[_-]+/).join(' ')}</td>
           </tr>
         ))}
       </tbody>
