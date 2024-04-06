413e9927ed62bd73e75d174db8cf7a4ffe4428e0
Anurag Lodhi <anuraglodhi63@gmail.com>
2024-04-06 17:04:31 +0530

    feat: slider for all categories

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { UserConfig, defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}) satisfies UserConfig;
